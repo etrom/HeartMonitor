c7b666fe9cc61bf819c29ef3ffde12ca26f48a49
martin butler <martinebutler@gmail.com>
2014-11-23 00:27:20 -0500

    install angular-strap. add dropdown menus for adding points to bars

--- client/app/app.js
+++ client/app/app.js
@@ -6,7 +6,8 @@ angular.module('barsApp', [
   'ngSanitize',
   'btford.socket-io',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'mgcrea.ngStrap'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
--- client/app/bars/bars.controller.js
+++ client/app/bars/bars.controller.js
@@ -1,43 +1,39 @@
 'use strict';
 
 angular.module('barsApp')
+  
+  // config for the dropdown button.
+  .config(function($dropdownProvider) {
+    angular.extend($dropdownProvider.defaults, {
+      html: true
+    });
+  })
+
   .controller('BarCtrl', function ($scope, Auth, $http, $log) {
     $scope.currentUser = Auth.getCurrentUser();
     $scope.bars = $scope.currentUser.bars;
     $scope.test = 'keep your voice down';
+    $scope.plusButtonPressed = '';
 
-      $scope.status = {
-        isopen: false
-      };
+    // values for the points dropdown button
+    $scope.dropdown = [
+      {text: '<i class="fa fa-plus"></i>&nbsp;10', click: 'addPercent(10)'},
+      {text: '<i class="fa fa-plus"></i>&nbsp;20', click: 'addPercent(20)'},
+      {text: '<i class="fa fa-plus"></i>&nbsp;30', click: 'addPercent(30)'} 
+    ];
 
-      $scope.$watch('status', function(oldval, newval) {
-        console.log(oldval);
-        console.log(newval);
-      });
+    // determine and record which bar's plus dropdown has been accessed
+    $scope.plusButtonClicked = function(barName) {
+      console.log('ding', barName);
+      $scope.plusButtonPressed = barName;
+    }
 
-      $scope.toggled = function(open) {
-        $log.log($scope.status);
-        $scope.status.isopen = !$scope.status.isopen;
-        $log.log($scope.status);
-      };
-
-      $scope.toggleDropdown = function($event) {
-        $event.preventDefault();
-        $event.stopPropagation();
-        // console.llog
-        $scope.status.isopen = !$scope.status.isopen;
-        // console.log($scope.status)
-      };
-
-
-      // increase fulfillment #'s
-        $scope.addPercent = function(barName, num){
-          console.log($scope.currentUser._id, 'user');
-          console.log(barName, 'name');
-          console.log(num, 'num')
-          $http.post('/api/users/bar/' + $scope.currentUser._id, { barName: barName, fulfillment: num});
-          console.log('added');
-
-        }
-
+    // increase fulfillment #'s
+    $scope.addPercent = function(num){
+      console.log($scope.currentUser._id, 'user');
+      console.log($scope.plusButtonPressed, 'name');
+      console.log(num, 'num')
+      $http.post('/api/users/bar/' + $scope.currentUser._id, { barName: $scope.plusButtonPressed, fulfillment: num});
+      console.log('added');
+    }
   });
