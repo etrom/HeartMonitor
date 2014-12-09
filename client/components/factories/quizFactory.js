1b065b8f0ecc0f099cde68385231246d243eae09
Elaine Trombley <elaine.trombley3@gmail.com>
2014-12-09 13:29:19 -0500

    quiz number increments

--- client/components/factories/quizFactory.js
+++ client/components/factories/quizFactory.js
@@ -1,13 +1,14 @@
 angular.module('barsApp')
         .factory('quizFactory', function(Auth, $http, $stateParams, $window) {
             return {
-                barPercentRequest: function(num, historyId) {
+                barPercentRequest: function(num, historyId, quizNum) {
                     var updateIncrease = num;
                     var user = Auth.getCurrentUser();
                     var partner = user.partner;
                     var history = historyId;
+                    var quizNum = quizNum;
                     console.log(history, 'history')
-                        $http.post('/api/users/requests/', {userId: partner._id, actionType: 'nwQuiz', increment: updateIncrease, historyId: history}).success(function(user){
+                        $http.post('/api/users/requests/', { userId: partner._id, actionType: 'nwQuiz', increment: updateIncrease, historyId: history, quizNum: quizNum}).success(function(user){
                             console.log(user, "new request")
                         });
                 }
